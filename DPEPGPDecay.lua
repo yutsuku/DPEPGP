789b5a322c47888e0237683f20874e7ae1ec37cc
moh <moh@yutsuku.net>
2017-10-09 17:28:48 +0200

    Changed the way that EP/GP notes gets updated. Kronos can't handle 500+ queries at once.

--- DPEPGPDecay.lua
+++ DPEPGPDecay.lua
@@ -69,23 +69,144 @@ end
 
 --function DPEPGP_DecayEP_OnClick()
 function DPEPGP_DecayEP_OnClick(decay)
+	this:Disable();
+	getglobal('DPEPGP_Decay_EPValue'):ClearFocus()
+	getglobal('DPEPGP_Decay_QueryLimit'):SetTextColor(0.5, 0.5, 0.5)
+	getglobal('DPEPGP_Decay_QueryLimit'):ClearFocus()
+	getglobal('DPEPGP_Decay_QueryLimit'):EnableMouse(false)
+	getglobal('DPEPGP_Decay_QueryInvertal'):SetTextColor(0.5, 0.5, 0.5)
+	getglobal('DPEPGP_Decay_QueryInvertal'):ClearFocus()
+	getglobal('DPEPGP_Decay_QueryInvertal'):EnableMouse(false)
+
 	DPEPGP_flushEPGP();
 	DPEPGP_AddGuild_OnClick();
---	local decay= DPEPGP_DecayInput:GetText();
 	DPEPGP_DecayEP(decay);
-	local msg = "All players' EP decayed by " .. decay .. "%!";
-	DPEPGP_Broadcast(msg,"G");
 end
 
 --function DPEPGP_DecayGP_OnClick()
 function DPEPGP_DecayGP_OnClick(decay)
+	this:Disable();
+	getglobal('DPEPGP_Decay_GPValue'):ClearFocus()
+	getglobal('DPEPGP_Decay_QueryLimit'):SetTextColor(0.5, 0.5, 0.5)
+	getglobal('DPEPGP_Decay_QueryLimit'):ClearFocus()
+	getglobal('DPEPGP_Decay_QueryLimit'):EnableMouse(false)
+	getglobal('DPEPGP_Decay_QueryInvertal'):SetTextColor(0.5, 0.5, 0.5)
+	getglobal('DPEPGP_Decay_QueryInvertal'):ClearFocus()
+	getglobal('DPEPGP_Decay_QueryInvertal'):EnableMouse(false)
+	
 	DPEPGP_flushEPGP();
 	DPEPGP_AddGuild_OnClick();
---	local decay= DPEPGP_DecayInput:GetText();
 	DPEPGP_DecayGP(decay);
-	local msg = "All players' GP decayed by " .. decay .. "%!";
-	DPEPGP_Broadcast(msg,"G");
 end
+
+local epgp_frame = CreateFrame('Frame')
+
+epgp_frame:Hide()
+epgp_frame.elapsed = 0
+epgp_frame.invertal = 1
+epgp_frame.execLimit = 50
+epgp_frame.TYPE_EP = 0
+epgp_frame.TYPE_GP = 1
+epgp_frame.operationType = -1
+epgp_frame.pct = 0
+epgp_frame.newData = {}
+
+function epgp_frame:push(t)
+	if getn(self.newData) > 0 then
+		error('Update is still processing. Aborting')
+		return
+	end
+	
+	self.newData = t
+end
+
+function epgp_frame:setType(newType, percentage)
+	if getn(self.newData) > 0 then
+		error('Update is still processing. Aborting')
+		return
+	end
+	
+	if newType == self.TYPE_GP then
+		self.operationType = self.TYPE_GP
+		self.pct = tonumber(percentage) or 0
+	elseif newType == self.TYPE_EP then
+		self.operationType = self.TYPE_EP
+		self.pct = tonumber(percentage) or 0
+	else
+		error('Unknown Decay type')
+	end
+end
+
+function epgp_frame:next()
+	if getn(self.newData) <= 0 then
+		ChatFrame1:AddMessage('Decay finished - wait for guild message to confirm.')
+		
+		if self.operationType == self.TYPE_EP then
+			DPEPGP_Broadcast("All players' EP decayed by " .. self.pct .. "%!", "G");
+		elseif self.operationType == self.TYPE_GP then
+			DPEPGP_Broadcast("All players' GP decayed by " .. self.pct .. "%!", "G")
+		end
+
+		self.elapsed = 0
+		this:Hide()
+		DPEPGP_DecayButton:Enable()
+		DPEPGP_Decay_QueryLimit:SetTextColor(1,1,1)
+		DPEPGP_Decay_QueryLimit:EnableMouse(true)
+		DPEPGP_Decay_QueryInvertal:SetTextColor(1,1,1)
+		DPEPGP_Decay_QueryInvertal:EnableMouse(true)
+	end
+	
+	
+	if self.operationType == self.TYPE_EP then
+		-- EP decay
+		local rows = 1
+		for i = getn(self.newData), 1, -1 do
+			if rows >= DPEPGP_QueryOptions.limit then
+				return
+			end
+			
+			DPEPGP_AddEPTo(self.newData[i].name, self.newData[i].value)
+			self.newData[i] = nil
+			
+			rows = rows + 1
+		end
+	elseif self.operationType == self.TYPE_GP then
+		-- GP decay
+		local rows = 1
+		for i = getn(self.newData), 1, -1 do
+			if rows >= DPEPGP_QueryOptions.limit then
+				return
+			end
+			
+			DPEPGP_AddGPTo(self.newData[i].name, self.newData[i].value)
+			self.newData[i] = nil
+			
+			rows = rows + 1
+		end
+	end
+end
+
+epgp_frame:SetScript('OnUpdate', function()
+	this.elapsed = this.elapsed + arg1
+	if this.elapsed >= DPEPGP_QueryOptions.invertal then
+		this.elapsed = 0
+		this:next()
+	end
+end)
+
+epgp_frame:RegisterEvent('ADDON_LOADED')
+epgp_frame:SetScript('OnEvent', function()
+	if event == 'ADDON_LOADED' and arg1 == 'DPEPGP' then
+		if not DPEPGP_QueryOptions then
+			DPEPGP_QueryOptions = {}
+			DPEPGP_QueryOptions.invertal = 1
+			DPEPGP_QueryOptions.limit = 50
+		end
+		
+		getglobal('DPEPGP_Decay_QueryLimit'):SetText(DPEPGP_QueryOptions.limit)
+		getglobal('DPEPGP_Decay_QueryInvertal'):SetText(DPEPGP_QueryOptions.invertal)
+	end
+end)
 
 function DPEPGP_DecayEP(percentage)
 	-- percentage must be a value between 0 and 100 -- DANGER: hardcoded baseEP here and in DPEPGP_AddGuild()!
@@ -94,13 +215,21 @@ function DPEPGP_DecayEP(percentage)
 	local name=nil;
 	local baseEP=0;
 	local newValue=0;
+	local newData = {}
 	while i <= GetNumGuildMembers() do
 		name = GetGuildRosterInfo(i);
 		newValue = (DPEPGP_GetEP(name)-baseEP)*multiplier;
 		newValue = math.floor(newValue);
-		DPEPGP_AddEPTo(name, -newValue);
+		--DPEPGP_AddEPTo(name, -newValue);
+		newData[i] = {}
+		newData[i].name = name;
+		newData[i].value = -newValue;
 		i=i+1;
 	end
+	ChatFrame1:AddMessage(format('EP Decay will be done in about %ds.', ceil(GetNumGuildMembers()/epgp_frame.execLimit*epgp_frame.invertal) ))
+	epgp_frame:setType(epgp_frame.TYPE_EP, percentage);
+	epgp_frame:push(newData);
+	epgp_frame:Show();
 	return;
 end
 
@@ -111,12 +240,20 @@ function DPEPGP_DecayGP(percentage)
 	local name=nil;
 	local baseGP=400; --hardcoded
 	local newValue=0;
+	local newData = {}
 	while i <= GetNumGuildMembers() do
 		name = GetGuildRosterInfo(i);
 		newValue = (DPEPGP_GetGP(name)-baseGP)*multiplier;
 		newValue= math.floor(newValue);
-		DPEPGP_AddGPTo(name, -newValue);
+		--DPEPGP_AddGPTo(name, -newValue);
+		newData[i] = {}
+		newData[i].name = name;
+		newData[i].value = -newValue;
 		i=i+1;
 	end
+	ChatFrame1:AddMessage(format('GP Decay will be done in about %ds.', ceil(GetNumGuildMembers()/epgp_frame.execLimit*epgp_frame.invertal) ))
+	epgp_frame:setType(epgp_frame.TYPE_GP, percentage);
+	epgp_frame:push(newData);
+	epgp_frame:Show();
 	return;
 end	
